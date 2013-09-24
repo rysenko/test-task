8a05d12a18df028a6f73e35843da9e64d644c57d
Alexander Rysenko <rysenko@gmail.com>
2013-09-24 21:07:37 +0300

    Fix assertions for palindrome specs

--- test/palindromeSpec.js
+++ test/palindromeSpec.js
@@ -1,20 +1,19 @@
-var assert = require('assert');
 var palindrome = require('../palindrome');
 
 describe('palindrome', function () {
     it('word', function () {
-        assert(palindrome('anna'), 'Anna is a palindrome');
+        expect(palindrome('anna')).toBeTruthy();
     });
     it('phrase with commas', function () {
-        assert(palindrome('Anne, I vote more cars race Rome to Vienna'), 'Rome to Vienna phrase is palindrome');
+        expect(palindrome('Anne, I vote more cars race Rome to Vienna')).toBeTruthy();
     });
     it('long phrase with dot', function () {
-        assert(palindrome('Are we not pure? “No sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man; a prisoner up to new era.'));
+        expect(palindrome('Are we not pure? “No sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man; a prisoner up to new era.')).toBeTruthy();
     });
     it('russian palindrome', function () {
-        assert(palindrome('А роза упала на лапу Азора?'));
+        expect('А роза упала на лапу Азора?').toBeTruthy();
     });
     it('not a palindrome', function () {
-        assert(!palindrome('Amore, Roman.'), 'Amore, Roman isn\'t a palindrome');
+        expect(palindrome('Amore, Roman.')).toBeFalsy();
     });
 });
